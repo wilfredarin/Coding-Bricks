fc187badc02d8461bafd29fa30cf8d6e8370b5cb
sameer ranjan <40149707+wilfredarin@users.noreply.github.com>
2019-10-03 23:50:58 +0530

    Just a Try not final

---
+++ aggressive cow.py
@@ -0,0 +1,49 @@
+# your code goes here
+def is_possible(cow,array,x):
+    i = 0
+    j = 1
+    cow =cow-1
+    while j < len(array) and cow >0 :
+        if array[j]-array[i]>=x:
+            cow -= 1
+            i = j
+            j += 1
+        else:
+            j += 1
+    if cow > 0:
+        return False
+    else:
+        return True
+
+def aggresive_cow(n,cow,array):
+
+    array.sort()
+    n-=1
+    gap = array[n] - array[0]
+    r = 0
+    l = gap
+    
+    ans = []
+    mid = (r+l)//2
+    while  r <= l:
+        mid = (r+l)//2
+       
+        if is_possible(cow,array,mid):
+            
+            ans.append(mid)
+            r = mid+1
+           
+        else:
+            l = mid -1 
+    print(max(ans))
+    
+t  = int(input())
+a = []
+for i in range(t):
+	a = []
+	n,cow = map(int,input().split())
+	for i in range(n):
+		a.append(int(input()))
+	aggresive_cow(n,cow,a)
+	
+	
